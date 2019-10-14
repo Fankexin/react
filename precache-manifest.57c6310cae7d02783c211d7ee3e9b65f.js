bcc857362a16c395947673a812e2ee39d561fdf0
Fankexin <974094944@qq.com>
2019-10-14 15:33:02 +0800

    .

---
+++ precache-manifest.57c6310cae7d02783c211d7ee3e9b65f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde1af598e6aee45226ac476d33d4b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f40714cdb45fd14ead",
+    "url": "/static/css/main.a2b16def.chunk.css"
+  },
+  {
+    "revision": "6391ae8687e6b56aac07",
+    "url": "/static/js/2.06adc54f.chunk.js"
+  },
+  {
+    "revision": "b7f40714cdb45fd14ead",
+    "url": "/static/js/main.716624a6.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
